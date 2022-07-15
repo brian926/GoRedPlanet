2dbd4a2b6f5fcc5c840aa368e97f897d8fd83e5b
Brian Antunes <brian.antunes@yahoo.com>
2022-07-15 07:11:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GoRedPlanet
+ 
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
